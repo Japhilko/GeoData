6168c90b712b6d3e1ec8793e43d233fd05a42c62
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2015-03-16 10:48:14 +0100

    Update Code
    Close polygons

--- rcode/RasGIS/rcode/CloseLines.R
+++ rcode/RasGIS/rcode/CloseLines.R
@@ -1,44 +1,76 @@
-# http://cdr.eionet.europa.eu/de/eu/noise/df8/colvi7k8q
+# Close lines
+# Jan-Philipp Kolb
+# Mon Mar 16 10:46:03 2015
+
+
+#------------------------#
+# Libraries
+#------------------------#
+
 
 library(maptools)
 library(rgdal)
 library(Grid2Polygons)
 
+
+#------------------------#
+# Set paths
+#------------------------#
+
+
 data.path <- "D:/Daten/Daten/GeoDaten/georefum"
 
 adm.path <- "D:/Daten/Daten/GeoDaten/DEU_adm"
 
+#------------------------#
+# Get data
+#------------------------#
 
 setwd(data.path)
-
+# http://cdr.eionet.europa.eu/de/eu/noise/df8/colvi7k8q
 RLP <- readOGR("DE_RP_ag1_Aggroad_Lden.shp","DE_RP_ag1_Aggroad_Lden")
 
-setwd(adm.path)
-DEU <- readShapePoly("DEU_adm3.shp")
+#------------------------#
+# Close lines
+#------------------------#
 
-class(DEU)
 
 coords_df <- list()
 
 for (i in 1:length(RLP)){
-  coords_df[[i]] <- RLP@lines[[i]]@Lines[[1]]@coords 
-  cat(i , "/n")
+  coords_df_i <- RLP@lines[[i]]@Lines[[1]]@coords 
+  erg <- sum(coords_df_i[1,]-  coords_df_i[nrow(coords_df_i),])
+  if(erg!=0){
+    coords_df[[i]] <- Polygons(list(Polygon(rbind(coords_df_i,coords_df_i[1,]))),i)
+  }else{
+    coords_df[[i]] <- Polygons(list(Polygon(coords_df_i)),i)
+  }
+  cat(i , "\n")
 }
 
-erg <- vector()
-for(i in 1:length(coords_df)){
-  dfi <- coords_df[[i]]
-  erg[i] <- sum(dfi[1,]-  dfi[nrow(dfi),])
+SPs = SpatialPolygons(coords_df)
+
+SPDF = SpatialPolygonsDataFrame(SPs, data.frame(NAME = RLP@data$NAME, row.names = 1:length(coords_df)))
+
+
+Cnames <- colnames(RLP@data)
+
+for (i in 2:length(Cnames)){
+  eval(parse(text=paste("SPDF$",Cnames[i],"<-RLP$",Cnames[i],sep="")))  
 }
 
-ind <- which(erg!=0)
 
-RLP@lines[[ind[1]]]@Lines[[1]]@coords
+#------------------------#
+# Control
+#------------------------#
 
+i <- 1
+
+i<- i+1
+plot(SPDF[i,])
 
 #------------------------#
 # Links
 #------------------------#
-
 
 # http://www.maths.lancs.ac.uk/~rowlings/Teaching/UseR2012/cheatsheet.html
