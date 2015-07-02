2b4e3b21ac9c1e18ea1787fbe94647f3d880a364
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2015-07-02 15:45:23 +0200

    Rheinau

--- geosmdata2/R/geocode_osm.R
+++ geosmdata2/R/geocode_osm.R
@@ -1,11 +1,15 @@
-geocode_osm <- function(place) {
+geocode_osm <- function(place,output=c("coord","all")){
+  output <- match.arg(output)
   place <- str_replace_all(place, ' ', '+')
   place <- URLencode(place)
   url_string <- str_c('http://nominatim.openstreetmap.org/search?format=json&addressdetails=1&q=', place)
   con <- url(url_string)
   geoc <- fromJSON(paste(readLines(con,warn=F), collapse = ''))
   close(con)
-  return(geoc)
+  geoc1 <- geoc[[1]]
+  geoc2 <- c(as.numeric(geoc1[which(names(geoc1)=="lat")]),as.numeric(geoc1[which(names(geoc1)=="lon")]))
+  names(geoc2) <- c("lat","lon")
+  if(output=="all"){return(geoc[[1]])}else{return(geoc2)}
 }
 
 
