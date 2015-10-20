b214ab86164e9c9ae45e0bfca069419092e0a4a6
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2015-10-20 13:55:22 +0200

    Web Scraping

--- 2015/rcode/SpatMA_WebScraping.R
+++ 2015/rcode/SpatMA_WebScraping.R
@@ -3,9 +3,15 @@
 # Jan-Philipp Kolb
 # Tue Oct 13 08:30:11 2015
 
+
+data.path <- "J:/Work/Statistik/Kolb/Workshops/2015/Spatial_MA/data/NurseryData"
+setwd(data.path)
+
 ## ------------------------------------------------------------------------
 # install.packages("XML")
 library(XML)
+
+
 
 ## ----eval=F--------------------------------------------------------------
 l<-"http://www.pflegesuche.de/bundesland_pflegeheime.html"
@@ -48,3 +54,37 @@ DEU1@data$nursingHomes <- df_pflege$Anz[ind]
 library(sp)
 spplot(DEU1,"nursingHomes")
 
+#----------------------------------------------#
+
+maketable <- function(tab){
+  tab1 <- gsub("\t","",tab)
+  tab2 <- strsplit(tab1,"\n")
+  dat <- unlist(lapply(tab2,function(x)x[1]))
+  bla <- unlist(lapply(tab2,function(x)x[2]))
+  df_pflege <- data.frame(place=bla,
+                          Anz=as.numeric(as.character(dat)))
+}
+
+
+l<-"http://www.pflegesuche.de/bundesland_pflegeheime_BB.html"
+ab <- readHTMLList(l)
+df_pflege <- maketable(ab[[7]])
+save(df_pflege,file="BB_df_pflege.RData")
+
+l <- "http://www.pflegesuche.de/bundesland_pflegeheime_HE.html"
+ab <- readHTMLList(l)
+df_pflege <- maketable(ab[[7]])
+save(df_pflege,file="HE_df_pflege.RData")
+
+#----------------------------------------------#
+# nr of people who need care per federal state
+#----------------------------------------------#
+
+l2 <- "https://www.gbe-bund.de/oowa921-install/servlet/oowa/aw92/dboowasys921.xwdevkit/xwd_init?gbe.isgbetol/xs_start_neu/&p_aid=3&p_aid=62857843&nummer=834&p_sprache=D&p_indsp=141&p_aid=27002330"
+
+#----------------------------------------------#
+# links
+#----------------------------------------------#
+
+
+# http://www.r-bloggers.com/htmltotext-extracting-text-from-html-via-xpath/
