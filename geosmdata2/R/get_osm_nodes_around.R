6e5420ceb0bd852654e2aa5cd59d900edd54ca36
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2015-10-23 14:40:19 +0200

    update nodes

--- geosmdata2/R/get_osm_nodes_around.R
+++ geosmdata2/R/get_osm_nodes_around.R
@@ -1,8 +1,12 @@
+
+# [maxsize:1073741824][timeout:900];node(51.15,7.0,51.35,7.3);out;
+
 get_osm_nodes_around <- function(place,radius=5){
-  Link1 <- "http://www.overpass-api.de/api/interpreter?data=node[name=\""
+  Link1 <- "http://www.overpass-api.de/api/interpreter?data=[maxsize:1073741824][timeout:900];node[name=\""
   InfoList <- xmlParse(paste(Link1,place,"\"];node(around:",radius,");out;",sep=""))
   return(InfoList)
 }
+
 
 
 # http://www.overpass-api.de/api/interpreter?data=area[name=\"Mannheim\"];node(around:5;out;
