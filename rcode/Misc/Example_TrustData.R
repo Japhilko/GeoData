fd24450a24827d07274124d2c45cad927def8779
Jan-Philipp Kolb <Jan-Philipp.Kolb@gesis.org>
2015-11-07 13:09:03 +0100

    update

--- rcode/Misc/Example_TrustData.R
+++ rcode/Misc/Example_TrustData.R
@@ -1,26 +1,26 @@
-Datapath <- "C:/Users/Floor/SkyDrive/Sociologie/Spacial social science/Data"
-setwd(Datapath)
-read.csv("Trust in others.csv")
-library(sp)
-Trust <-read.csv("Trust in others.csv",sep=";")
-install.packages("tmap")
-library(tmap)
-data(Europe)
-qtm(Europe)
-matchmap <- match(Trust[,2],Europe@data$name)
+# Visualize trust data
+# Floor Veerman and Jan-Philipp Kolb
+# Sat Nov 07 12:56:03 2015
 
-Europe@data$name
+library(sp)
+# install.packages("tmap")
+library(tmap)
+
+link <- "https://raw.githubusercontent.com/Japhilko/GeoData/master/data/TrustData.csv"
+
+Trust <- read.csv2(link,sep=";")
 
 Trust[,2] <- as.character(Trust[,2])
+Trust[,4] <- as.character(Trust[,4])
+Trust[,4] <- gsub(",",".",Trust[,4])
+Trust[,4] <- as.numeric(Trust[,4])
+
+data(Europe)
+
 Trust[,2][Trust[,2]=='Czech Republic'] <- "Czech Rep."
 
-matchmap <- match(Trust[,2],Europe@data$name)
-
-
 matchmap2 <- match(Europe@data$name,Trust[,2])
-Europe@data$Trust <- as.numeric(Trust[matchmap2,4])
+Europe@data$Trust <- as.numeric(as.character(Trust[matchmap2,4]))
 
 qtm(Europe,'Trust')
 
-Trust[is.na(matchmap),2]
-sort(Europe@data$name)
