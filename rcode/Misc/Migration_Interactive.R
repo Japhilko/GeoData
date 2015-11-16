bf83fb9c419bab1604d436c2d1b5d700378c4d19
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2015-11-16 08:42:45 +0100

    update interactive maps

--- rcode/Misc/Migration_Interactive.R
+++ rcode/Misc/Migration_Interactive.R
@@ -1,7 +1,40 @@
 # Jan-Philipp Kolb
 # Fri Nov 13 19:22:07 2015
 
+library(magrittr)
+library(leaflet)
+
+#---------------------------------------------#
+# load data
+#---------------------------------------------#
+
 
 url <- "https://raw.githubusercontent.com/Japhilko/
 GeoData/master/2015/data/RefugeePlaces.csv"
-Places <- read.csv(url)
+Refugees <- read.csv(url)
+
+#---------------------------------------------#
+# Interactive map
+#---------------------------------------------#
+
+
+content <- paste(sep = "<br/>",
+                 "<b><a href='",Refugees$Link,"'>",
+                 Refugees$Place,"</a></b>",
+                 Refugees$description,
+                 Refugees$direction)
+
+m2 <- leaflet() %>%
+  addTiles() %>%
+  addMarkers(lng=Refugees$lon,
+             lat=Refugees$lat,
+             popup=content)
+
+m2
+
+#---------------------------------------------#
+# Interactive map
+#---------------------------------------------#
+
+
+# https://rstudio.github.io/leaflet/popups.html
