b014a74ec9b43a550570d60202610a9776b03152
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2016-02-23 11:28:34 +0100

    update rmd

--- geosmdata/R/hello.R
+++ geosmdata/R/hello.R
@@ -1,18 +1,20 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+get_osm_nodes <- function(object2,place){
+  ind <- which(object2==osm_df$Value)
+  if(length(ind)>0){
+    ind2 <- agrep("node",osm_df$Element[ind])
+    if(length(ind2)>0){
+      ind <- ind[ind2[1]]
+      type_obj <- "node"
+      object <- paste(osm_df$Key[ind],"=",object2,sep="")
+      Link1 <- "http://www.overpass-api.de/api/interpreter?data=[maxsize:1073741824][timeout:900];area[name=\""
+      InfoList <- xmlParse(paste(Link1,place,"\"];",type_obj,"(area)[",object,"];out;",sep=""))
+      return(InfoList)
+    }else{
+      ind2 <- agrep("way",osm_df$Element[ind])
+      if(length(ind2)>0){stop("Try function get_osm_ways")
+      }else{"Try function get_osm_relation"}
 
-hello <- function() {
-  print("Hello, world!")
+    }
+
+  }else{stop("Map feature not found - have a look at http://wiki.openstreetmap.org/wiki/Map_Features")}
 }
