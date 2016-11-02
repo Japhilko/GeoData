88b92f0d71e70f14be1a09df82be930d6bc5b476
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2016-11-02 14:50:43 +0100

    delete old version of package

--- geosmdata/R/get_osm_nodes.R
+++
@@ -1,6 +0,0 @@
-get_osm_nodes <- function(object,place){
-  Link1 <- "http://www.overpass-api.de/api/interpreter?data=[maxsize:1073741824][timeout:900];area[name=\""
-  InfoList <- xmlParse(paste(Link1,place,"\"];node(area)[",object,"];out;",sep=""))
-  return(InfoList)  
-}
-
