ce15972241ad6b82ab61301edba374a35356a28b
Japhilko <JanPhilipp.kolb@gmail.com>
2021-05-22 22:06:39 +0200

    plz

--- rcode/PLZ_Umkreissuche.R
+++ rcode/PLZ_Umkreissuche.R
@@ -4,6 +4,8 @@
 
 
 data_path <- "C:/Arbeit/data/plz-gebiete.shp/"
+plz_path <- "C:/Arbeit/GESIS/presentations/20151101_LifBi_Georeferenzierung/data/KiTaData/"
+graph_path <- "C:/github/GeoData/presentations/"
 
 
 # R Pakete laden ----------------------------------------------------------
@@ -23,9 +25,23 @@ plz <- readShapePoly(paste0(data_path,"plz-gebiete.shp"))
 
 plz6 <- plz[substr(plz@data$plz,1,1)==6,]
 
-p6 <- coordinates(plz)
+p6 <- coordinates(plz6)
 
-distances <- pointDistance(p6,lonlat = T)
+distances <- pointDistance(p6,lonlat = F)
+
+p_color <- distances[1,]
+
+p_color <- p_color/max(p_color,na.rm = T)
+
+p_color <- rgb(0,0,p_color)
+
+
+pdf(paste0(graph_path,"plz_umkreis.pdf"))
+  plot(plz6,col=p_color,border="lightgray")
+dev.off()
+
+
+
 
 # Links -------------------------------------------------------------------
 
