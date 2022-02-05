ddbb435799f622dda4c6317f3403761530f74346
Japhilko <JanPhilipp.kolb@gmail.com>
2022-02-05 08:34:47 +0100

    working with spark

--- rcode/working_with_r_spark.R
+++ rcode/working_with_r_spark.R
@@ -3,6 +3,28 @@
 # spark_install("2.3")
 
 library(sparklyr)
+library(DBI)
+library(dplyr)
 
 # https://therinspark.com/starting.html
 # spark_install("2.3")
+
+sc <- spark_connect(master = "local", version = "2.3")
+cars <- copy_to(sc, mtcars)
+
+
+# spark_web(sc)
+
+
+dbGetQuery(sc, "SELECT count(*) FROM mtcars")
+
+
+
+?spark_apply
+
+
+
+# Streaming
+
+dir.create("input")
+write.csv(mtcars, "input/cars_1.csv", row.names = F)
